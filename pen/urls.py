c6f2386a38489f49ca21f5cb98421eef3d37a000
arjunpradeep <arjunpradeepsp@gmail.com>
2022-01-12 11:53:07 +0530

    try

---
+++ pen/urls.py
@@ -0,0 +1,5 @@
+from django.urls import
+from.import views
+urlpatterns[
+    path('max/',views.pen1)
+]
---
+++ pen/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def pen1(request):
+    return render(request,'max.html')
